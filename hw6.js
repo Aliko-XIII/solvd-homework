5761bc71f8b0e4711fd2ace76cd3780256a0ee17
Aliko-XIII <tektov2004@gmail.com>
2024-05-01 12:39:21 +0300

    task 1 update

--- hw6.js
+++ hw6.js
@@ -19,23 +19,49 @@ const translations = {
     }
 };
 
-const language = "fr"; // Change to "en" for English
+const language = "en"; // Change to "en" for English
 const greeting = "greet";
 const introduction = "intro";
 
-const localize = (strings, exp) => {
+const localize = (strings, ...phrases) => {
     {
-        if (!translations[language]) { throw new Error('Wrong language identifier.') }
-        if (!translations[language][exp]) { throw new Error('Wrong phrase identifier.') }
-        return `${translations[language][exp]}`;
+        // if (!translations[language]) { throw new Error('Wrong language identifier.') }
+        let res = '';
+        let curString;
+        let curPhrase;
+        for (let i = 0; i < Math.max(strings.length, phrases.length); i++) {
+            curString = strings[i] !== undefined ? strings[i] : '';
+            try {
+                curPhrase = phrases[i] !== undefined ? `${translations[language][phrases[i]]}` : '';
+            }
+            catch (e){
+                curPhrase = '';
+                console.log(e);
+            }
+            res += curString + curPhrase;
+        }
+        return res;
     }
 }
 
-const localizedGreeting = localize`${greeting}`;
-const localizedIntroduction = localize`${introduction}`;
+const localizedGreeting = localize`UserName, ${greeting}!`;
+const localizedIntroduction = localize`${introduction} :)`;
 {
     {
         console.log(localizedGreeting); // Expected: "Bonjour" (for language "fr")
         console.log(localizedIntroduction); // Expected: "Bienvenue sur notre site web" (for language "fr")
     }
+}
+
+// Task 2: Advanced Tagged Template
+
+// Create a function called highlightKeywords that acts as
+//  a tagged template. The function should take a template
+//  string and an array of keywords. It should highlight each
+//  occurrence of a keyword in the template by wrapping it in
+//  a <span> element with a specific CSS class. Use template
+//  literals and string manipulation to achieve this.
+
+const highlightKeywords = (strings) => {
+
 }
