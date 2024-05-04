8c143f26b09c94eecfcd06bebd0ec4ca444a97b3
Aliko-XIII <tektov2004@gmail.com>
2024-05-04 15:06:55 +0300

    finished, except challenge

--- hw6.js
+++ hw6.js
@@ -223,4 +223,58 @@ const throttledScrollHandler = throttle(onScroll, 1000);
 // window.addEventListener("scroll", throttledScrollHandler);
 
 
+// Task 6: Currying Function Implementation
+// Your task is to implement a currying function that converts a given
+// function into a curried version. Currying is a technique in which 
+// a function that takes multiple arguments is transformed into a
+// sequence of functions, each taking a single argument.
+
+// Instructions
+
+// Implement a function called curry that takes two arguments:
+// func: The function to be curried.
+// arity: The number of arguments the original function takes.
+
+// The curry function should return a new curried function.
+
+// The curried function should keep accepting arguments until
+//  it has received the specified number of arguments (arity).
+//  Once all arguments are received, the original function should
+//  be executed with the collected arguments.
+
+// The curried function should keep accepting arguments until it
+//  has received the specified number of arguments (arity). Once
+//  all arguments are received, the original function should be 
+// executed with the collected arguments.
+/**
+ * 
+ * @param {Function} func 
+ * @param {number} arity 
+ */
+function curry(func, arity) {
+    const args = [];
+    return curried = (arg) => {
+        args.push(arg);
+        if (args.length === arity) {
+            return func(...args);
+        }
+        else {
+            return curried;
+        }
+    };
+
+}
+
+function multiply(a, b, c) {
+    return a * b * c;
+}
+
+const curriedMultiply = curry(multiply, 3);
+
+const step1 = curriedMultiply(2); // Returns a curried function
+const step2 = step1(3); // Returns a curried function
+const result = step2(4); // Returns the final result: 2 * 3 * 4 = 24
+
+{ { console.log("Result:", result); } }
+// Expected: 24
 
