533170b3cf1f5c49f4d006414be028f8b0d81988
Aliko-XIII <tektov2004@gmail.com>
2024-05-04 15:07:37 +0300

    minor

--- hw6.js
+++ hw6.js
@@ -275,6 +275,5 @@ const step1 = curriedMultiply(2); // Returns a curried function
 const step2 = step1(3); // Returns a curried function
 const result = step2(4); // Returns the final result: 2 * 3 * 4 = 24
 
-{ { console.log("Result:", result); } }
-// Expected: 24
+console.log("Result:", result); // Expected: 24
 
