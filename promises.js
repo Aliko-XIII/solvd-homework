4321a427ba80c7eb60515990c612264862249d7c
Aliko-XIII <tektov2004@gmail.com>
2024-05-08 11:35:52 +0300

    task2

--- promises.js
+++ promises.js
@@ -50,3 +50,64 @@ promiseAll(promises)
     .catch(error => {
         console.error("At least one promise rejected:", error);
     });
+
+// Task 2: Implement promiseAllSettled Function
+
+// Your task is to implement a function called promiseAllSettled that mimics the
+// behavior of Promise.allSettled().The function should accept an array of promises
+// and return a promise that resolves to an array of objects representing the
+// settlement of each promise
+
+// Instructions
+//  Implement a function called promiseAllSettled that takes an array of promises
+// as an argument.
+//  The function should return a new promise that resolves with an array of
+// objects representing the settlement of each promise in the input array.
+//  Each object in the resolved array should have
+// properties status and value or reason . The status can be
+// either 'fulfilled' or 'rejected' , and value should hold the resolved value (if
+// fulfilled) or reason should hold the rejection reason (if rejected).
+
+/**
+ * 
+ * @param {Array<Promise>} promises 
+ */
+const promiseAllSettled = async (promises) =>
+    new Promise((resolve, reject) => {
+        if (promises.length === 0) {
+            resolve([]);
+        }
+        const res = []
+        let counter = 0;
+        promises.forEach((promise, index) => {
+            promise.then(val => {
+                res[index] = {
+                    status: 'fulfilled',
+                    value: val
+                };
+            }).catch(reason => {
+                res[index] = {
+                    status: 'rejected',
+                    reason: reason
+                };
+            }).finally(() => {
+                counter++;
+                if (counter == promises.length) {
+                    resolve(res);
+                }
+            })
+        });
+    })
+
+const promises2 = [
+    Promise.resolve(1),
+    Promise.reject("Error occurred"),
+    Promise.resolve(3)
+];
+promiseAllSettled(promises2)
+    .then(results => {
+        console.log("All promises settled:", results);
+        // Expected: [{ status: 'fulfilled', value: 1 },
+        // { status: 'rejected', reason: 'Error occurred' },
+        // { status: 'fulfilled', value: 3 }]
+    });
