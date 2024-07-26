de07c0b5ce35eacf034e17ca7e56c0358218197c
Aliko-XIII <tektov2004@gmail.com>
2024-07-26 12:12:30 +0300

    added comments

--- personalProj/controllers/authorizationController.js
+++ personalProj/controllers/authorizationController.js
@@ -8,8 +8,9 @@ require('dotenv').config();
 const secret = process.env.SECRET;
 
 /**
- * 
- * @param {string} token 
+ * Validates the signature of a JWT token
+ * @param {string} token - JWT token to be validated
+ * @returns {boolean} - true if the signature is valid, false otherwise
  */
 function validateSignature(token) {
     const [headerEncoded, payloadEncoded, signature] = token.split('.');
@@ -20,6 +21,11 @@ function validateSignature(token) {
     return signature === testSignature;
 }
 
+/**
+ * Creates an access token for the given user
+ * @param {User} user - User object
+ * @returns {string} - Generated access token
+ */
 function createAccessToken(user) {
     const accessHeader = {
         "alg": "HS256",
@@ -29,9 +35,7 @@ function createAccessToken(user) {
     };
 
     const accessHeaderEncoded = Buffer.from(JSON.stringify(accessHeader)).toString('base64url');
-
     const accessPayloadEncoded = Buffer.from(JSON.stringify(user)).toString('base64url');
-
     const signature = crypto.createHmac('sha256', secret)
         .update(accessHeaderEncoded + '.' + accessPayloadEncoded).digest('base64url');
 
@@ -39,6 +43,11 @@ function createAccessToken(user) {
     return accessToken;
 }
 
+/**
+ * Creates a refresh token for the given user
+ * @param {User} user - User object
+ * @returns {string} - Generated refresh token
+ */
 function createRefreshToken(user) {
     const refreshHeader = {
         "alg": "HS256",
@@ -46,27 +55,36 @@ function createRefreshToken(user) {
         "iat": (new Date()).toString(),
         "exp": "3d"
     };
+
     const refreshHeaderEncoded = Buffer.from(JSON.stringify(refreshHeader)).toString('base64url');
-
-    const refreshPayloadEncoded = Buffer.from(
-        JSON.stringify({ "id": user.id })).toString('base64url');
-
+    const refreshPayloadEncoded = Buffer.from(JSON.stringify({ "id": user.id })).toString('base64url');
     const refreshSignature = crypto.createHmac('sha256', secret)
         .update(refreshHeaderEncoded + '.' + refreshPayloadEncoded).digest('base64url');
 
     const refreshToken = `${refreshHeaderEncoded}.${refreshPayloadEncoded}.${refreshSignature}`;
-    return refreshToken
+    return refreshToken;
 }
 
+/**
+ * Updates the refresh token in the database for the given user
+ * @param {string} user_id - User ID
+ * @param {string} refresh_token - Refresh token
+ * @returns {Promise<void>}
+ */
 async function updateRefresh(user_id, refresh_token) {
     const expires_at = (new Date(getExpiration(refresh_token))).toISOString();
     const res = await query(`INSERT INTO refresh_tokens (user_id, refresh_token, expires_at)
     VALUES ('${user_id}', '${refresh_token}', '${expires_at}')
     ON CONFLICT (user_id)
     DO UPDATE SET refresh_token = EXCLUDED.refresh_token, expires_at = EXCLUDED.expires_at;`);
-
 }
 
+/**
+ * Checks if the refresh token is valid for the given user
+ * @param {string} user_id - User ID
+ * @param {string} refresh_token - Refresh token
+ * @returns {Promise<boolean>} - true if the refresh token is valid, false otherwise
+ */
 async function isRefreshValid(user_id, refresh_token) {
     const res = await query(`SELECT * FROM refresh_tokens
         WHERE user_id = '${user_id}'
@@ -74,6 +92,9 @@ async function isRefreshValid(user_id, refresh_token) {
     return res.rows.length !== 0 && refresh_token === res.rows[0].refresh_token;
 }
 
+/**
+ * Handles user login, generates access and refresh tokens
+ */
 async function loginUser(req, res) {
     try {
         const user = await User.getUserByPhone(req.body['phone']);
@@ -96,22 +117,37 @@ async function loginUser(req, res) {
     } catch (err) {
         res.status(500).send(err);
     }
-};
+}
 
+/**
+ * Extracts the header from a JWT token
+ * @param {string} token - JWT token
+ * @returns {object} - Decoded header
+ */
 function getTokenHeader(token) {
     const headerEncoded = token.split('.')[0];
     const headerDecoded = Buffer.from(headerEncoded, 'base64url').toString();
     const header = JSON.parse(headerDecoded);
-    return header
+    return header;
 }
 
-const getTokenPayload = token => {
+/**
+ * Extracts the payload from a JWT token
+ * @param {string} token - JWT token
+ * @returns {object} - Decoded payload
+ */
+function getTokenPayload(token) {
     const payloadEncoded = token.split('.')[1];
     const payloadDecoded = Buffer.from(payloadEncoded, 'base64url').toString();
     const payload = JSON.parse(payloadDecoded);
-    return payload
+    return payload;
 }
 
+/**
+ * Gets the expiration time of a JWT token
+ * @param {string} token - JWT token
+ * @returns {number} - Expiration time in milliseconds since the epoch
+ */
 function getExpiration(token) {
     const { iat, exp } = getTokenHeader(token);
     const issuedAt = new Date(iat);
@@ -121,13 +157,17 @@ function getExpiration(token) {
     let expiresAt;
     if (expMeasure === 'm') {
         expiresAt = issuedAt.getTime() + expNumber * 1000 * 60;
-    }
-    else if (expMeasure === 'd') {
+    } else if (expMeasure === 'd') {
         expiresAt = issuedAt.getTime() + expNumber * 1000 * 60 * 60 * 24;
     }
     return expiresAt;
 }
 
+/**
+ * Checks if a JWT token is expired
+ * @param {string} token - JWT token
+ * @returns {boolean} - true if the token is expired, false otherwise
+ */
 function isTokenExpired(token) {
     const { iat, exp } = getTokenHeader(token);
     const issuedAt = new Date(iat);
@@ -136,22 +176,29 @@ function isTokenExpired(token) {
     let difference;
     if (expMeasure === 'm') {
         difference = ((new Date()).getTime() - issuedAt.getTime()) / 1000 / 60;
-    }
-    else if (expMeasure === 'd') {
+    } else if (expMeasure === 'd') {
         difference = ((new Date()).getTime() - issuedAt.getTime()) / 1000 / 60 / 60 / 24;
     }
     return (expNumber < difference);
 }
 
+/**
+ * Refreshes the access token using a valid refresh token
+ */
 async function refreshAccessToken(req, res) {
     try {
         const refreshToken = req.body.refresh_token;
-        !refreshToken ? res.send('No refresh token in request') : null;
+        if (!refreshToken) {
+            res.send('No refresh token in request');
+            return;
+        }
 
         const id = getTokenPayload(refreshToken).id;
         const isValid = await isRefreshValid(id, refreshToken);
-        !validateSignature(refreshToken) || !isValid ?
-            res.status(400).send('Refresh token is not valid') : null;
+        if (!validateSignature(refreshToken) || !isValid) {
+            res.status(400).send('Refresh token is not valid');
+            return;
+        }
 
         const user = await User.getUserById(id);
         res.send({
@@ -160,18 +207,22 @@ async function refreshAccessToken(req, res) {
     } catch (err) {
         res.status(500).send(err);
     }
-};
+}
 
+/**
+ * Middleware to validate the authorization token
+ */
 async function validateToken(req, res, next) {
     const isValid = req.headers.authorization
         && validateSignature(req.headers.authorization)
         && !isTokenExpired(req.headers.authorization);
 
-    isValid ?
-        next() :
+    if (isValid) {
+        next();
+    } else {
         res.status(500).send('Authorization is not valid!');
+    }
 }
-
 
 module.exports = {
     loginUser,
