aed3bbe3af7ee93c054ccb7ad38879d9c362c5b4
Aliko-XIII <tektov2004@gmail.com>
2024-08-23 09:02:12 +0300

    user, symptom and organ route tests

---
+++ personalProj/test/routes/symptomRoutes.test.js
@@ -0,0 +1,93 @@
+const request = require('supertest');
+const express = require('express');
+const routes = require('../../routes/symptomRoutes');
+const { Symptom } = require('../../models/Symptom');
+
+const app = express();
+app.use(express.json());
+app.use('/api/symptoms', routes);
+
+const symptomFields = {
+    name: 'Headache',
+    description: 'A continuous pain in the head.',
+};
+let symptomId = null;
+let symptomProperties = [];
+
+describe('GET /symptoms', () => {
+    const testSymptom = new Symptom(symptomFields.name, symptomFields.description);
+    symptomProperties = Object.getOwnPropertyNames(testSymptom).filter(prop =>
+        typeof testSymptom[prop] !== 'function');
+    test('should return an array of symptoms', async () => {
+        const res = await request(app).get('/api/symptoms');
+        expect(res.statusCode).toEqual(200);
+
+        res.body.forEach(symptom => {
+            symptomProperties.forEach(prop => expect(symptom).toHaveProperty(prop));
+        });
+    });
+
+    test('should return an array of symptoms with filter by name', async () => {
+        const res = await request(app).get('/api/symptoms?name=a');
+        expect(res.statusCode).toEqual(200);
+        res.body.forEach(symptom => {
+            symptomProperties.forEach(prop => expect(symptom).toHaveProperty(prop));
+            expect(symptom.name.indexOf('a')).toBeTruthy();
+        });
+    });
+});
+
+describe('POST /symptoms', () => {
+    test('should insert a symptom object to DB', async () => {
+        const res = await request(app).post('/api/symptoms').send({
+            name: symptomFields.name,
+            description: symptomFields.description
+        });
+        expect(res.statusCode).toEqual(201);
+        symptomId = res.body.id;
+        expect(res.body.id > 0).toBeTruthy();
+        symptomProperties.forEach(prop => expect(res.body).toHaveProperty(prop));
+    });
+
+});
+
+describe('GET /symptoms/:id', () => {
+    test('should return a symptom from DB', async () => {
+        const res = await request(app).get('/api/symptoms/' + symptomId);
+        expect(res.statusCode).toEqual(200);
+        Object.keys(symptomFields).forEach(key => expect(res.body[key]).toEqual(symptomFields[key]));
+        symptomProperties.forEach(prop => expect(res.body).toHaveProperty(prop));
+    });
+
+    test('should return 500 status for invalid symptom id', async () => {
+        const res = await request(app).get('/api/symptoms/'+'badId');
+        expect(res.statusCode).toEqual(500);
+    });
+});
+
+describe('UPDATE /symptoms/:id', () => {
+    test('should update a symptom in DB', async () => {
+        const res = await request(app).put('/api/symptoms/' + symptomId).send({
+            name: 'newName',
+            description: 'newDescription'
+        });;
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.name).toEqual('newName');
+        expect(res.body.description).toEqual('newDescription');
+        symptomProperties.forEach(prop => expect(res.body).toHaveProperty(prop));
+    });
+
+    test('should return 400 status for invalid field value', async () => {
+        const res = await request(app).put('/api/symptoms/' + symptomId).send({
+            name: ''
+        });;
+        expect(res.statusCode).toEqual(400);
+    });
+});
+
+describe('DELETE /symptoms/:id', () => {
+    test('should delete a symptom from DB', async () => {
+        const res = await request(app).delete('/api/symptoms/' + symptomId);
+        expect(res.statusCode).toEqual(204);
+    });
+});
---
+++ personalProj/test/routes/userRoutes.test.js
@@ -0,0 +1,113 @@
+const request = require('supertest');
+const express = require('express');
+const routes = require('../../routes/userRoutes');
+const { User } = require('../../models/User');
+
+const app = express();
+app.use(express.json());
+app.use('/api/users', routes);
+
+const userFields = {
+    firstName: 'TestFirst',
+    lastName: 'TestLast', 
+    phone: '11111111111',
+    password: 'testpass',
+    age: 25,
+    sex: 'M',
+};
+let userId = null;
+let userProperties=[];
+
+describe('GET /users', () => {
+    const testUser = new User(userFields.firstName, userFields.lastName, userFields.phone,
+        userFields.password, userFields.age, userFields.sex);
+    userProperties = Object.getOwnPropertyNames(testUser).filter(prop =>
+        typeof testUser[prop] !== 'function' && prop !== 'password'
+    );
+    test('should return an array of users', async () => {
+        const res = await request(app).get('/api/users');
+        expect(res.statusCode).toEqual(200);
+
+        res.body.forEach(user => {
+            userProperties.forEach(prop => expect(user).toHaveProperty(prop));
+        });
+    });
+
+    test('should return an array of users with filter by sex', async () => {
+        const res = await request(app).get('/api/users?sex=M');
+        expect(res.statusCode).toEqual(200);
+        res.body.forEach(user => {
+            userProperties.forEach(prop => expect(user).toHaveProperty(prop));
+            expect(user.sex).toEqual('M');
+        });
+    });
+    test('should return an array of users with filter by sex', async () => {
+        const res = await request(app).get('/api/users?minAge=20&maxAge=40');
+        expect(res.statusCode).toEqual(200);
+        res.body.forEach(user => {
+            userProperties.forEach(prop => expect(user).toHaveProperty(prop));
+            expect(user.age >= 20 && user.age <= 40).toBeTruthy();
+        });
+    });
+});
+
+describe('POST /users', () => {
+    test('should insert a user object to DB', async () => {
+        const res = await request(app).post('/api/users').send({
+            firstName: userFields.firstName,
+            lastName: userFields.lastName,
+            phone: userFields.phone,
+            password: userFields.password,
+            age: userFields.age,
+            sex: userFields.sex
+        });
+        expect(res.statusCode).toEqual(201);
+        userId = res.body.id;
+        expect(res.body.id.length > 0).toBeTruthy();
+        userProperties.forEach(prop => expect(res.body).toHaveProperty(prop));
+    });
+
+});
+
+describe('GET /users/:id', () => {
+    test('should return a user from DB', async () => {
+        const res = await request(app).get('/api/users/' + userId);
+        expect(res.statusCode).toEqual(200);
+        Object.keys(userFields).forEach(key => expect(res.body[key]).toEqual(userFields[key]));
+        userProperties.forEach(prop => expect(res.body).toHaveProperty(prop));
+    });
+
+    test('should return 500 status for invalid user id', async () => {
+        const res = await request(app).get('/api/users/'+'badId');
+        expect(res.statusCode).toEqual(500);
+    });
+});
+
+describe('UPDATE /users/:id', () => {
+    test('should update a user in DB', async () => {
+        const res = await request(app).put('/api/users/' + userId).send({
+            firstName: 'NewFirst',
+            age: 26,
+            sex: 'F'
+        });;
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.firstName).toEqual('NewFirst');
+        expect(res.body.age).toEqual(26);
+        expect(res.body.sex).toEqual('F');
+        userProperties.forEach(prop => expect(res.body).toHaveProperty(prop));
+    });
+
+    test('should return 400 status for invalid field value', async () => {
+        const res = await request(app).put('/api/users/' + userId).send({
+            sex: 'INVALID'
+        });;
+        expect(res.statusCode).toEqual(400);
+    });
+});
+
+describe('DELETE /users/:id', () => {
+    test('should delete a user from DB', async () => {
+        const res = await request(app).delete('/api/users/' + userId);
+        expect(res.statusCode).toEqual(204);
+    });
+});
