be22bbf1698188304b9bb2a30c005335245f30f7
Aliko-XIII <tektov2004@gmail.com>
2024-09-01 18:03:21 +0300

    db config changes for docker

--- personalProj/config/database.js
+++ personalProj/config/database.js
@@ -3,20 +3,20 @@ const fs = require('fs');
 const { Client } = require('pg');
 
 const client = new Client({
-    // user: process.env.DB_USER,
-    // host: process.env.DB_HOST,
-    // database: process.env.DB_DATABASE,
-    // password: process.env.DB_PASSWORD,
-    // port: process.env.DB_PORT,
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'postgres',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    // user: 'postgres',
+    // host: 'localhost',
+    // database: 'postgres',
+    // password: 'postgres',
+    // port: 5432,
 });
 
 client.connect()
-    // .then(() => console.log('Connected to the PostgreSQL'))
+    .then(() => console.log('Connected to the PostgreSQL'))
     .catch(err => console.error('Connection error', err.stack));
 
 function query(queryStr) {
@@ -34,7 +34,7 @@ async function createDatabase() {
 
     try {
         await query(sqlScript);
-        // console.log('Database created successfully');
+        console.log('Database created successfully');
     } catch (err) {
         console.error('Error creating database', err.stack);
     }
